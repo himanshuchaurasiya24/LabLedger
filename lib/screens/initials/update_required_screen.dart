8e612ba34d1770b642c264adf984edfb52787c77
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2025-10-07 22:14:59 +0530

    added url launching for update

--- lib/screens/initials/update_required_screen.dart
+++ lib/screens/initials/update_required_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:labledger/screens/ui_components/reusable_ui_components.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UpdateRequiredScreen extends StatelessWidget {
   final String requiredVersion;
   const UpdateRequiredScreen({super.key, required this.requiredVersion});
+
+  Future<void> _launchURL(String urlString) async {
+    final Uri url = Uri.parse(urlString);
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      debugPrint('Could not launch $urlString');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +36,17 @@ class UpdateRequiredScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Text(
-              'A new version of the app is available. Please update to\nversion $requiredVersion to continue.',
+              'A new version of the app is available. Please update to\nversion v$requiredVersion to continue.',
               style: theme.textTheme.bodyLarge,
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 32),
             ReusableButton(
-              text: 'Contact Support to Update',
+              text: 'Click here to download',
               onPressed: () {
-              
+                _launchURL(
+                  "https://github.com/himanshuchaurasiya24/LabLedger/releases/",
+                );
               },
               width: 253,
               variant: ButtonVariant.elevated,
